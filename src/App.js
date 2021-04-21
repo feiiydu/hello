1829824c85e086f953116d099c1115e4623a2b19
Fei <apitsada.s@kbtg.tech>
2021-04-21 13:17:19 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Nonggap was here!       
+      </header>
+    </div>
+  );
+}
+
+export default App;
