0dca4b2fa5d0af1a0eb5f21a4142adab9e2568ef
Fei <apitsada.s@kbtg.tech>
2021-04-21 15:38:18 +0700

    hello nonggap

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,19 +10,27 @@ import { Component } from "react";
 class Form extends Component {
     constructor(){
         super();
-        this.state = {data: ""};
-        this.doPrinting = this.doPrinting.bind(this);
+        this.state = {data: "",message: ""};
+        this.doPrintingT = this.doPrinting.bind(this);
     }
     
 doPrinting(event) {
-    this.setState({data :  this.state.data + event.key});
+    this.setState({name : event.target.value});
+}
+SayHello(event) {
+    console.log("Click Submit!!!")
+    this.setState({message : "Hello " + this.state.name});
 }
 
     render() {
         return (
             <div>
-                <input type="text" onKeyPress={this.doPrinting} />
-                <p>Hello : {this.state.data}</p>
+                <input 
+                    type="text" 
+                    onKeyUp={this.doPrintingT} 
+                />
+                <button onClick={() => this.SayHello()}>Submit!!!</button>
+                <p>{this.state.message}</p>
             </div>
         );
     }
